9c57a9e4ce3c3510cffb0cdb8544927bf2cc4fc6
Amila Kalansooriya <amila@ittex.com.au>
2013-01-31 15:15:01 +0530

    test commit
    fsdfssd fd dfgd

---
+++ doc/channa.sql
@@ -0,0 +1,7 @@
+select tblJobs.fldId,tblJobs.fldCustCode,fldJobCode,fldTest,fldNoSamples,fldReportContact,fldEmailAddress,fldIsCopyOnly from tblReportingDetails, tblRepAddressJobs, tblJobs 
+where tblReportingDetails.fldRepId = tblRepAddressJobs.fldRepId 
+and tblJobs.fldCustCode = tblReportingDetails.fldCustCode
+and fldCancel='false'
+and tblReportingDetails.fldCustCode = 'DBR10'
+and fldTest is not null
+order by tblReportingDetails.fldCustCode,tblJobs.fldId,fldJobCode,fldTest,fldisCopyOnly
